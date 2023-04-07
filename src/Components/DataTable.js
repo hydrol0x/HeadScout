7f24cab9fa4be1cdfe69614d2ff5c8a49bd8a3ed
hydrol0x <mrjacobry@gmail.com>
2023-04-07 11:49:33 -0400

    useEffect to automatically fetch data once the page loads

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -1,44 +1,23 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Table, Button } from "react-bootstrap";
 
 const DataTable = () => {
-  const [sheetsData, setSheetsData] = useState([
-    {
-      "Match Number": "0",
-      "Team Number": "0",
-      "Left Community in Auto?": "",
-      "Scored Game Pieces in Auto? [Cone Top]": "0",
-      "Scored Game Pieces in Auto? [Cube Top]": "0",
-      "Scored Game Pieces in Auto? [Cone Middle]": "0",
-      "Scored Game Pieces in Auto? [Cube Middle]": "0",
-      "Scored Game Pieces in Auto? [Cone Bottom]": "0",
-      "Scored Game Pieces in Auto? [Cube Bottom]": "0",
-      "Auto Charging Station": "No data about auto charge station yet!",
-      "Teleop Cones Scored Top": "0",
-      "Teleop Cones Scored Mid": "0",
-      "Teleop Cones Scored Bottom": "0",
-      "Teleop Cubes Scored Top": "0",
-      "Teleop Cubes Scored Mid": "0",
-      "Teleop Cubes Scored Bottom": "0",
-      "Teleop Cones Dropped": "0",
-      "Teleop Cubes Dropped": "0",
-      "End Game": "",
-    },
-  ]);
+  const [sheetsData, setSheetsData] = useState([{}]);
 
-  const handleClick = async (e) => {
+  const handleSheetsData = async (e) => {
     const data = await window.sheetsAPI.getSheet();
-    console.log(data);
     setSheetsData(data);
   };
 
-  //   sheetsData.map((match) => {
-  //     Object.values(match).map((value) => console.log(value));
-  //   });
+  // get data on DOM load
+  useEffect(() => {
+    handleSheetsData();
+  }, []);
+
   return (
     <div>
-      <Button className="mb-2" onClick={handleClick} variant="primary">
+      <Button className="mb-2" onClick={handleSheetsData} variant="primary">
         Reload
       </Button>
 
